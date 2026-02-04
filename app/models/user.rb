6373436847fd9a447e29ee44c58c4277b52b7dd8
Jacob <jacoblittler2003@gmail.com>
2026-02-04 14:48:44 -0500

    Edit user and new users are complete and saving to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true
-  validates :password, prescence: true
+  validates :password, presence: true
 end
